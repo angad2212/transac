05a804dc7fdbc69c9e9a20d77b59936756eff28f
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2025-02-07 16:01:00 +0530

    mongoose call

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,4 @@
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
-dotenv.config();
-
-const MONGODB_URL = process.env.MONGODB_URL
-
-mongoose.connect(MONGODB_URL)
 
 const userSchema = new mongoose.Schema({
     username: {
--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,32 @@ const express = require("express");
 const rootRouter = require("./routes/index.js")
 const cors = require("cors");
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config();
 
 const PORT = process.env.PORT;
-//It allows your server to accept requests from different origins (other domains).
-//since our frontend runs on a seperate server
 
 const app = express();
 
 app.use(cors());
+//It allows your server to accept requests from different origins (other domains).
+//since our frontend runs on a seperate server
+
 app.use(express.json());
+
+const connectDB = async () => {
+  try {
+      const conn = await mongoose.connect(process.env.MONGO_URL);
+      console.log('Connected to MongoDB database');
+  } catch (error) {
+      console.log(`Error: ${error.message}`);
+      process.exit(1);
+  }
+};
+
+//calling the function to connect to database
+connectDB();
 
 app.use("/api/v1", rootRouter) //This line tells your Express application 
 //to use the rootRouter for any requests that start with /api/v1.
